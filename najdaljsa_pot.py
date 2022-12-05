6cb8b64d8221d3fd9ebc1caea004b236180b759f
Val Groleger <val.groleger@gmail.com>
2022-12-05 12:48:30 +0100

    simulacija za DFS in color coding

--- najdaljsa_pot.py
+++ najdaljsa_pot.py
@@ -1,5 +1,7 @@
 from collections import defaultdict
 import random
+import csv
+import time
 
 def nakljucni_graf(dolzina_max=10, utez_min=0, utez_max=6):
     dolzina = random.randint(0, dolzina_max)
@@ -15,6 +17,21 @@ def nakljucni_graf(dolzina_max=10, utez_min=0, utez_max=6):
         graf[vozlisce] = povezave
     return graf
 
+def nakljucni_graf_neutezen(dolzina_max=10):
+    dolzina = dolzina_max
+    graf = {}
+    for vozlisce in range(0, dolzina):
+        povezave = []
+        for sosed in range(0, vozlisce):
+            povezava = random.randint(0,1)
+            if povezava == 1:
+                povezave.append(sosed)
+        for sosed in range(vozlisce+1, dolzina):
+            povezava = random.randint(0,1)
+            if povezava == 1:
+                povezave.append(sosed)
+        graf[vozlisce] = povezave
+    return graf
 
 def nasprotni_graf(graf):
     novi_graf = {}
@@ -84,12 +101,9 @@ for (s,t) in robovi:
 #print("Dolžina najdaljše poti:")
 #print(max_dolzina)
 
-def color_coding(G,u,K,n = 100): 
-
+def color_coding(G,K,n = 100): 
     ### G graf, n število ponovitev barvanj pri vsakem K, K največje število barv
-
     odgovor = 0
-
     for k in range(2,K+1): ###stevilo barv, ki jih preverjamo
         j = 0
         while j < n: ###da imamo n ponovitev za vsako število barv
@@ -100,17 +114,11 @@ def color_coding(G,u,K,n = 100):
             x = {}
             mnozice = [frozenset({j for j in range(1, k+1) if (i >> (j-1)) & 1 == 1}) for i in range(2**k)]
             mnozice = sorted([s for s in mnozice if len(s) > 1], key=len)
-            #dol = 1
-           # y = True
+
             for v in G:
                 for i in range(1,k+1):
                     x[v,frozenset({i})] = barvanje[v] == i
             for C in mnozice:
-                #if len(C) > dol:
-                   # if not y:
-                   #     break
-                    #dol += 1
-                   # y = False
                 for v in G:
                     b = barvanje[v]
                     x[v, C] = b in C and any(x[u, C - {b}] for u in G[v])
@@ -119,4 +127,39 @@ def color_coding(G,u,K,n = 100):
                 odgovor = k
     return odgovor
 
-print(color_coding(G,"1", 7,5))
+def stevilo_povezav(G):
+    c = 0
+    for el in G:
+        c += len(G[el])
+    return c
+
+def simulacija(st_vozlisc,st_barv,st_grafov = 15, st_ponovitev = 20):
+    datoteka = f"simulacija_{st_vozlisc}_{st_barv}_{st_ponovitev}.csv"
+    with open(datoteka, "w") as f:
+        writer = csv.writer(f)
+        writer.writerow(["Število povezav", "Graf", "Dolžina najdaljše poti - Color coding", "Čas", "Dolžina najdaljše poti - DFS", "Čas"])
+        for i in range(1, st_grafov+1):
+            G = nakljucni_graf_neutezen(st_vozlisc)
+            st_pov = stevilo_povezav(G)
+            start1= time.time()
+            dolzina1 = color_coding(G, st_barv, st_ponovitev)
+            end1 = time.time()
+            cas1 = end1 - start1
+            start2 = time.time()
+            vse_poti = [p for ps in [DFS(G, n) for n in set(G)] for p in ps]
+            dolzina2   = max(len(p) for p in vse_poti)
+            end2 = time.time()
+            cas2 = end2 - start2
+            writer.writerow([st_pov, G, dolzina1, cas1, dolzina2, cas2 ])
+            f.flush()
+ 
+
+#simulacija(5,9)
+#simulacija(6,10)
+#simulacija(7,10)
+#simulacija(8,11)
+#simulacija(9,12)
+#simulacija(10,13)
+#simulacija(11,14)
+#simulacija(12,15)
+#simulacija(13,16) 
