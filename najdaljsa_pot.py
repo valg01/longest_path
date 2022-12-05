c3722261eef55451b202e831ec900a75eea17ba4
Val Groleger <val.groleger@gmail.com>
2022-12-05 17:29:59 +0100

    Dodane simulacije

--- najdaljsa_pot.py
+++ najdaljsa_pot.py
@@ -3,8 +3,7 @@ import random
 import csv
 import time
 
-def nakljucni_graf(dolzina_max=10, utez_min=0, utez_max=6):
-    dolzina = random.randint(0, dolzina_max)
+def nakljucni_graf_topoloski(dolzina, utez_min=0, utez_max=6):
     graf = {}
     for vozlisce in range(0, dolzina):
         povezave = {}
@@ -153,6 +152,42 @@ def simulacija(st_vozlisc,st_barv,st_grafov = 15, st_ponovitev = 20):
             writer.writerow([st_pov, G, dolzina1, cas1, dolzina2, cas2 ])
             f.flush()
  
+def simulacija_med_dvema_vozliscema(st_vozlisc, min_utez, max_utez, st_grafov = 15):
+    datoteka = f"simulacija_{st_vozlisc}_{min_utez}_{max_utez}.csv"
+    with open(datoteka, "w") as f:
+        writer = csv.writer(f)
+        writer.writerow(["Število povezav", "najvecja dovoljena utez", "najmanjsa dovoljena utez", "Graf", "Dolžina najdaljše poti med prvim in zadnjim vozliscem", "Čas"])
+        for i in range(1, st_grafov+1):
+            G = nakljucni_graf_topoloski(st_vozlisc, min_utez, max_utez)
+            st_pov = stevilo_povezav(G)
+            start1= time.time()
+            dolzina1 = najdaljsa_pot(G)
+            end1 = time.time()
+            cas1 = end1 - start1
+            writer.writerow([st_pov, max_utez, min_utez, G, dolzina1, cas1,])
+            f.flush()
+
+def simulacija_st_ponovitev(st_ponovitev, preskok = 5, st_grafov = 1, st_vozlisc = 8, st_barv = 11):
+    datoteka = f"simulacija_st_ponovitev_{st_vozlisc}_{st_barv}_{st_ponovitev}_{preskok}.csv"
+    with open(datoteka, "w") as f:
+        writer = csv.writer(f)
+        writer.writerow(["Število povezav", "Graf", "Število ponovitev","Dolžina najdaljše poti - Color coding", "Čas", "Dolžina najdaljše poti - DFS", "Čas"])
+        for i in range(1, st_grafov+1):
+            G = nakljucni_graf_neutezen(st_vozlisc)
+            st_pov = stevilo_povezav(G)
+            start2 = time.time()
+            vse_poti = [p for ps in [DFS(G, n) for n in set(G)] for p in ps]
+            dolzina2   = max(len(p) for p in vse_poti)
+            end2 = time.time()
+            cas2 = end2 - start2
+            for n in range(1,st_ponovitev, preskok):
+                start1= time.time()
+                dolzina1 = color_coding(G, st_barv,n)
+                end1 = time.time()
+                cas1 = end1 - start1
+                writer.writerow([st_pov, G, n, dolzina1, cas1, dolzina2, cas2 ])
+                f.flush()
+    
 
 #simulacija(5,9)
 #simulacija(6,10)
@@ -163,3 +198,11 @@ def simulacija(st_vozlisc,st_barv,st_grafov = 15, st_ponovitev = 20):
 #simulacija(11,14)
 #simulacija(12,15)
 #simulacija(13,16) 
+
+#simulacija_st_ponovitev(1000,20)
+
+# simulacija_med_dvema_vozliscema(10,1,8)
+# simulacija_med_dvema_vozliscema(10,-4,3)
+# simulacija_med_dvema_vozliscema(30,-2,2)
+# simulacija_med_dvema_vozliscema(30,1,1)
+# simulacija_med_dvema_vozliscema(100,-7,7)
